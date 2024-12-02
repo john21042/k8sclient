03381e915a8e05fe2964ff21296f3c5712d4304e
jlhavli <jlhavli@uwe.nsa.gov>
2024-12-02 20:16:56 +0000

    test

--- test/main_test.go
+++ test/main_test.go
@@ -1,5 +1,22 @@
+package main
 
+import (
+	//"fmt"
+	"log"
+	k8sclient "my/k8sclient"
+	//"k8s.io/client-go/kubernetes/fake"
+	// cmd "my/cmd"
+	"testing"
+)
+
+//var realInitK8sClient = k8sclient.InitK8sClient  // returns *kubernetes.Clientset
+var mockInitK8sClient = k8sclient.MockInitK8sClient
 
 func TestInitK8sClient(t *testing.T) {
-	kubeClient := fake.NewSimpleClientset()
+	log.Println("k8sclient/test/main_test.go")
+	kubeClient := mockInitK8sClient()
+	if kubeClient == nil {
+		log.Fatal("mockInitK8sClient Failed")
+	}
+	// assert.NotNil(kubeClient, "should not be nil")
 }
